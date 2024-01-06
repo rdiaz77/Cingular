97f46c67fefa4995058d53d684152368f83c0950
RDM_ <rafadiazm77@gmail.com>
2024-01-06 15:19:36 -0600

    access to db and show in views

--- purchase_orders/views.py
+++ purchase_orders/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from purchase_orders.models import Orders, Buyer, Seller, Product
+
+
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hola Mundo")
+    response = Orders.objects.all()
+    output = ", ".join([oc.name for oc in response])
+    # return HttpResponse('hola')
+    return HttpResponse(output)
     
+    
+
+    
+
