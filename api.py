4e1ffb021d9fd6e7bd5d02b99fd1d2de98f1af9e
RDM_ <rafadiazm77@gmail.com>
2024-01-06 23:13:07 -0600

    parsed json data from API

--- api.py
+++ api.py
@@ -7,10 +7,28 @@ from config import public_key
 url_oc = f"https://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?codigo=1627-1836-SE23&ticket={
     public_key}"
 
-response_oc = requests.get(url_oc).json()  # converts json to dict
-# print(response_oc)
-data_oc = response_oc
-print(data_oc)
+d = requests.get(url_oc).json()
+
+
+d_oc = d["Listado"][0]
+
+# 'FechaCreacion': '2023-12-01T10:41:02.777', 'FechaEnvio': '2023-12-13T10:37:18.837', 'FechaAceptacion': '2023-12-13T19:23:24.39', 'FechaCancelacion': None, 'FechaUltimaModificacion': '2023-12-14T16:38:00'}
+d_date = d["Listado"][0]["Fechas"]
+
+# {'CodigoOrganismo': '7448', 'NombreOrganismo': 'SERVICIO DE SALUD HOSPITAL DE SAN FERNANDO', 'RutUnidad': '61.602.145-1', 'CodigoUnidad': '2623', 'NombreUnidad': 'Hospital de San Fernando Abastecimiento', 'Actividad': 'HOSPITALES Y CLINICAS', 'DireccionUnidad': 'Negrete 1401', 'ComunaUnidad': 'San Fernando', 'RegionUnidad': 'Región del Libertador General Bernardo O´Higgins', 'Pais': 'CL', 'NombreContacto': 'Giovanni Gonzalez', 'CargoContacto': 'Administrativo Logistica', 'FonoContacto': '', 'MailContacto': ''}
+
+d_buyer = d["Listado"][0]["Comprador"]
+
+
+# {'CodigoOrganismo': '7448', 'NombreOrganismo': 'SERVICIO DE SALUD HOSPITAL DE SAN FERNANDO', 'RutUnidad': '61.602.145-1', 'CodigoUnidad': '2623', 'NombreUnidad': 'Hospital de San Fernando Abastecimiento', 'Actividad': 'HOSPITALES Y CLINICAS', 'DireccionUnidad': 'Negrete 1401', 'ComunaUnidad': 'San Fernando', 'RegionUnidad': 'Región del Libertador General Bernardo O´Higgins', 'Pais': 'CL', 'NombreContacto': 'Giovanni Gonzalez', 'CargoContacto': 'Administrativo Logistica', 'FonoContacto': '', 'MailContacto': ''}
+d_seller = d["Listado"][0]["Proveedor"]
+
+
+# 'Cantidad': 3, 'Listado': [{'Correlativo': 1, 'CodigoCategoria': 41116000, 'Categoria': 'Equipamiento para laboratorios / Instrumentos de medida y experimentación / Reactivos de analizadores clínicos y diagnósticos', 'CodigoProducto': 41116004, 'Producto': 'Reactivos de analizadores químicos', 'EspecificacionComprador': 'REACTIVO PARA DETERMINACIÓN DE NIVELES DE AMIKACINA (R1 2X19ML R2: 2X7 ML)\r\n', 'EspecificacionProveedor': 'REACTIVO PARA DETERMINACIÓN DE NIVELES DE AMIKACINA (R1 2X19ML R2: 2X7 ML)', 'Cantidad': 1.0, 'Unidad': None, 'Moneda': 'CLP', 'PrecioNeto': 899500.0, 'TotalCargos': 0.0, 'TotalDescuentos': 0.0, 'TotalImpuestos': 0.0, 'Total': 899500.0}, {'Correlativo': 2, 'CodigoCategoria': 41116000, 'Categoria': 'Equipamiento para laboratorios / Instrumentos de medida y experimentación / Reactivos de analizadores clínicos y diagnósticos', 'CodigoProducto': 41116004, 'Producto': 'Reactivos de analizadores químicos', 'EspecificacionComprador': 'TIRAS REACTIVAS PARA DETERMINACIÓN DE CETONEMIA (B-KETONE)(50 DETERMINACIONES C/U)', 'EspecificacionProveedor': 'TIRAS REACTIVAS PARA DETERMINACIÓN DE CETONEMIA (B-KETONE)(50 DETERMINACIONES C/U)', 'Cantidad': 4.0, 'Unidad': None, 'Moneda': 'CLP', 'PrecioNeto': 357500.0, 'TotalCargos': 0.0, 'TotalDescuentos': 0.0, 'TotalImpuestos': 0.0, 'Total': 1430000.0}, {'Correlativo': 3, 'CodigoCategoria': 41116000, 'Categoria': 'Equipamiento para laboratorios / Instrumentos de medida y experimentación / Reactivos de analizadores clínicos y diagnósticos', 'CodigoProducto': 41116004, 'Producto': 'Reactivos de analizadores químicos', 'EspecificacionComprador': 'CALIBRADOR PARA TÉCNICA AMIKACINA ( 6 NOVELES, 1X1 ML CADA UNO)', 'EspecificacionProveedor': 'CALIBRADOR PARA TÉCNICA AMIKACINA ( 6 NOVELES, 1X1 ML CADA UNO)', 'Cantidad': 1.0, 'Unidad': None, 'Moneda': 'CLP', 'PrecioNeto': 340000.0, 'TotalCargos': 0.0, 'TotalDescuentos': 0.0, 'TotalImpuestos': 0.0, 'Total': 340000.0}]}
+
+d_items = d["Listado"][0]["Items"]
+d_items_details = d["Listado"][0]["Items"]["Listado"][1]["PrecioNeto"]
+print(d_items_details)
 
 # for item in data_oc:
     # print(item["Fechas"])
@@ -20,8 +38,6 @@ print(data_oc)
     # # print(item["Items"])
     # buyer_oc = (item["Comprador"])
 
-# print(buyer_oc["RutUnidad"])
-# print(provider_oc["Codigo"])
 
 
 # Datos consulta: Cantidad, FechaCreacion, Version // response_oc
@@ -30,6 +46,9 @@ print(data_oc)
 # Proveedor: Codigo, Comuna, Region
 # Items : Cantidad, Correlativo, CodigoProducto, EspecificacionComprador, EspecificacionProveedor, Cantidad, Unidad, Moneda, PrecioNeto, TotalCargos, TotalDescuentos,TotalDescuentos, Total.
 
-# for item in orden_compra:
-#     print(item["Codigo"])
-# [item for item in list]
+
+# d = requests.get("http://ergast.com/api/f1/2019/drivers.json")
+# d = d.json()
+# print(d)
+# drivers = d["MRData"]["DriverTable"]["Drivers"]
+# print(f'this is drivers {drivers}')
--- sandbox.py
+++ sandbox.py
@@ -1,207 +1,214 @@
-from array import array
-from sys import getsizeof
-from pprint import pprint
+import requests
+import json
+# from array import array
+# from sys import getsizeof
+# from pprint import pprint
 
 
-# print("hello world")
-# print("Sofia " * 20)
+# # print("hello world")
+# # print("Sofia " * 20)
 
-# x = 2
-# y = 9
-# print(x+y)
-# print(x / y)
-# print(x % y)
-# print(12 / 3)
-# print(12 % 3)
-# fruit = "Apple"
-# print(fruit[-1])
-# for number in range(1, 10, 3):
-#     print("Attempt", number)
+# # x = 2
+# # y = 9
+# # print(x+y)
+# # print(x / y)
+# # print(x % y)
+# # print(12 / 3)
+# # print(12 % 3)
+# # fruit = "Apple"
+# # print(fruit[-1])
+# # for number in range(1, 10, 3):
+# #     print("Attempt", number)
 
-# count = 0
-# for x in range(1, 10):
-#     if x % 2 == 0:
-#         count += 1
-#         print(x)
-# print(f"we have {count} even numbers")
+# # count = 0
+# # for x in range(1, 10):
+# #     if x % 2 == 0:
+# #         count += 1
+# #         print(x)
+# # print(f"we have {count} even numbers")
 
 
-# def multiply(*numbers):
-#     total = 1
-#     for number in numbers:
-#         total *= number
-#     return total
+# # def multiply(*numbers):
+# #     total = 1
+# #     for number in numbers:
+# #         total *= number
+# #     return total
 
 
-# print(multiply(1, 2, 3, 4, 5, 10))
+# # print(multiply(1, 2, 3, 4, 5, 10))
 
-# value = multiply(1, 2, 3, 4, 5, 10)
-# print(f'this is the result: {value}')
+# # value = multiply(1, 2, 3, 4, 5, 10)
+# # print(f'this is the result: {value}')
 
-# def fizz_buzz(number):
-#     if number % 3 == 0 and number % 5 == 0:
-#         return "fizz_buzz"
-#     if number % 3 == 0:
-#         return "fizz"
-#     if number % 5 == 0:
-#         return "buzz"
-#     return number
+# # def fizz_buzz(number):
+# #     if number % 3 == 0 and number % 5 == 0:
+# #         return "fizz_buzz"
+# #     if number % 3 == 0:
+# #         return "fizz"
+# #     if number % 5 == 0:
+# #         return "buzz"
+# #     return number
 
 
-# print(fizz_buzz(7))
+# # print(fizz_buzz(7))
 
-# number_list = list(range(1, 21))
-# print(number_list)
+# # number_list = list(range(1, 21))
+# # print(number_list)
 
-# letters = list("hello")
+# # letters = list("hello")
 
-# print(letters)
+# # print(letters)
 
 
-items = [("P1", 10), ("P2", 20), ("P3", 45), ("P4", 5)]
-prices = []
-products = []
+# items = [("P1", 10), ("P2", 20), ("P3", 45), ("P4", 5)]
+# prices = []
+# products = []
 
-# for item in items:
-#     prices.append(item[1])
-# print(prices)
+# # for item in items:
+# #     prices.append(item[1])
+# # print(prices)
 
-# prices = list(map(lambda item: item[1], items))
+# # prices = list(map(lambda item: item[1], items))
 
-# print(f'lambda fuction: {prices}')
+# # print(f'lambda fuction: {prices}')
 
-# high_price = []
+# # high_price = []
 
-# for item in items:
-#     if item[1] >= 10:
-#         high_price.append(item)
+# # for item in items:
+# #     if item[1] >= 10:
+# #         high_price.append(item)
 
-# print(f' this is the long way {high_price}')
+# # print(f' this is the long way {high_price}')
 
-# h_price = list(filter(lambda item: item[1] >= 10, items))
-# print(f'thi is the short way {h_price}')
+# # h_price = list(filter(lambda item: item[1] >= 10, items))
+# # print(f'thi is the short way {h_price}')
 
-# point = (1, 22, 3, 4, 5, 6)
-# print(type(point))
-# point_list = list(point)
-# print(point_list)
-# print(type(point_list))
+# # point = (1, 22, 3, 4, 5, 6)
+# # print(type(point))
+# # point_list = list(point)
+# # print(point_list)
+# # print(type(point_list))
 
-numbers = array("i", [1, 2, 3, 4, 5])
+# numbers = array("i", [1, 2, 3, 4, 5])
 
-numbers.append(9)
-print(numbers)
-print(numbers[1])
+# numbers.append(9)
+# print(numbers)
+# print(numbers[1])
 
 
-points = dict(x=3, t=10, z=8, y=7)
-print(type(points))
+# points = dict(x=3, t=10, z=8, y=7)
+# print(type(points))
 
-for x in points:
-    print(x)
+# for x in points:
+#     print(x)
 
-print(points["t"])
-print(points.get("a"), 0)
+# print(points["t"])
+# print(points.get("a"), 0)
 
-for key, value in points.items():
-    print(value)
+# for key, value in points.items():
+#     print(value)
 
 
-values = []
+# values = []
 
-for x in range(5):
-    values.append(x * 2)
+# for x in range(5):
+#     values.append(x * 2)
 
-print(values)
-# in list comprehension syntax [expression for item in iterable if condition == True]
-# expression =  do something
-# item = loop variable
+# print(values)
+# # in list comprehension syntax [expression for item in iterable if condition == True]
+# # expression =  do something
+# # item = loop variable
 
-values = [x * 2 for x in range(5)]
-print(values)
+# values = [x * 2 for x in range(5)]
+# print(values)
 
-# generated objects in ()
+# # generated objects in ()
 
-# values_2 = (x * 2 for x in range(10000000))
-# print(values_2)
-# print(f'the size of value is {getsizeof(values_2)}')
+# # values_2 = (x * 2 for x in range(10000000))
+# # print(values_2)
+# # print(f'the size of value is {getsizeof(values_2)}')
 
-# numbers_3 = [1, 2, 3, 4, 5]
-# print(numbers_3)
-# print("value", numbers[0])
-# # unpacking operator *
+# # numbers_3 = [1, 2, 3, 4, 5]
+# # print(numbers_3)
+# # print("value", numbers[0])
+# # # unpacking operator *
 
-# print(*numbers_3)
+# # print(*numbers_3)
 
-# values_3 = [*range(5), *"hello world", *{1, 2, 8, 9}]
-# print(values_3)
+# # values_3 = [*range(5), *"hello world", *{1, 2, 8, 9}]
+# # print(values_3)
 
 
-sentence = "this is a common interview question"  # the most iterative letter
+# sentence = "this is a common interview question"  # the most iterative letter
 
 
-frequency = {}
+# frequency = {}
 
-for item in sentence:
-    if item in frequency:
-        frequency[item] += 1
-    else:
-        frequency[item] = 1
-print(frequency)
+# for item in sentence:
+#     if item in frequency:
+#         frequency[item] += 1
+#     else:
+#         frequency[item] = 1
+# print(frequency)
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-# the __str__ magic method allows printing certain values from the class. Note that "f" is outside the parenthesis when using return
+# # the __str__ magic method allows printing certain values from the class. Note that "f" is outside the parenthesis when using return
 
-    def __str__(self):
-        return f'({self.x} and {self.y})'
+#     def __str__(self):
+#         return f'({self.x} and {self.y})'
 
 
-# class method is useful to zero-out a class for example, and get back to preset/original values
+# # class method is useful to zero-out a class for example, and get back to preset/original values
 
-    @classmethod
-    def zero(cls):
-        return (0, 0)
+#     @classmethod
+#     def zero(cls):
+#         return (0, 0)
 
-    def draw(self):
-        print(f"point x: {self.x} and point y: {self.y}")
+#     def draw(self):
+#         print(f"point x: {self.x} and point y: {self.y}")
 
 
-point = Point(1, 4)
+# point = Point(1, 4)
 
-point.draw()
-print(f'y = {point.y}')
+# point.draw()
+# print(f'y = {point.y}')
 
-point_zero = Point.zero()
+# point_zero = Point.zero()
 
-print(point_zero)
+# print(point_zero)
 
-# Chat Gpt List methods, magic methods
+# # Chat Gpt List methods, magic methods
 
-print(str(point))
+# print(str(point))
 
-tags = {"python": 3, "java": 4, "js": 5}
+# tags = {"python": 3, "java": 4, "js": 5}
 
-print(type(tags))
+# print(type(tags))
 
-for tag in tags.values():
-    print(tag)
+# for tag in tags.values():
+#     print(tag)
 
 
-class Animal:
-    def eat(self):
-        print("eat")
+# class Animal:
+#     def eat(self):
+#         print("eat")
 
 
-class Bird(Animal):
-    def fly(self):
-        print("fly")
+# class Bird(Animal):
+#     def fly(self):
+#         print("fly")
 
 
-panther = Animal()
+# panther = Animal()
 
-print(panther.eat())
+# print(panther.eat())
+d = requests.get("http://ergast.com/api/f1/2019/drivers.json")
+d = d.json()
+print(d)
+# drivers = d["MRData"]["DriverTable"]["Drivers"]
+# print(f'this is drivers {drivers}')
