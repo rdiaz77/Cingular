4e15a5b12451b275cd7683352449035695e78b1c
RDM_ <rafadiazm77@gmail.com>
2024-01-07 17:01:22 -0600

    home page, add app_name to purchase_orders view

---
+++ api/api.py
@@ -0,0 +1,75 @@
+import json
+import requests
+from config import public_key
+
+
+
+url_oc = f"https://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?codigo=1627-1836-SE23&ticket={
+    public_key}"
+
+res = requests.get(url_oc).json()
+
+# entrega la full lista de informacion, aunque el dict base que genera json tiene informacion addicional:
+# Cantidad': 1, 'FechaCreacion': '2024-01-07T05:22:48.0319247Z', 'Version': 'v1'
+# {'Codigo': '1627-1836-SE23', 'Nombre': 'CINTAS DE CETONEMIA', 'CodigoEstado': 12, 'Estado': 'Recepción Conforme', 'CodigoLicitacion': '', 'Descripcion': 'SE REQUIERE PARA EL LABORATORIO DEL HOSPITAL DE SAN FERNANDO\r\n\r\nSBYS 1345\r\n\r\nSEGÚN COT N°2544', 'CodigoTipo': '8', 'Tipo': 'SE', 'TipoMoneda': 'CLP', 'CodigoEstadoProveedor': 7, 'EstadoProveedor': 'Recepción Conforme', 'Fechas': {'FechaCreacion': '2023-12-01T10:41:02.777', 'FechaEnvio': '2023-12-13T10:37:18.837', 'FechaAceptacion': '2023-12-13T19:23:24.39', 'FechaCancelacion': None, 'FechaUltimaModificacion': '2023-12-14T16:38:00
+
+res_oc = res["Listado"][0]
+
+def retrieved_oc_values():
+    for oc in res_oc:
+        r_oc_number = res_oc['Codigo']
+        r_oc_status = res_oc['CodigoEstado']
+        r_oc_name = res_oc['Nombre']
+        sql = 'INSERT INTO Purchase_orders_orders (number, status_code, name)'
+        val = (r_oc_number,r_oc_status,r_oc_name)
+        cur.execute(sql,val)
+
+print(r_oc_number)
+print(r_oc_status)
+print(r_oc_name)
+retrieved_oc_values()
+
+#  Insert data in sqlite
+# for dr in drivers:
+#     name = dr["familyName"]
+#     number = dr["permanentNumber"] 
+#     sql = 'INSERT INTO Drivers (name,number) VALUES(?,?)'
+#     val = (name,number)
+#     cur.execute(sql,val)
+
+
+# 'FechaCreacion': '2023-12-01T10:41:02.777', 'FechaEnvio': '2023-12-13T10:37:18.837', 'FechaAceptacion': '2023-12-13T19:23:24.39', 'FechaCancelacion': None, 'FechaUltimaModificacion': '2023-12-14T16:38:00'}
+
+res_date = res["Listado"][0]["Fechas"]
+
+for i in res_date:
+    f_creation = res_date['FechaCreacion']
+    if res_date['FechaCancelacion'] != None:
+        f_cancela = res_date['FechaCancelacion']
+    else:
+        f_cancela = "empty"
+
+print(f_cancela)
+print(f_creation)
+
+
+# {'CodigoOrganismo': '7448', 'NombreOrganismo':, 'RutUnidad': '61.602.145-1', 'CodigoUnidad': '2623', 'NombreUnidad': 'Hospital de San Fernando Abastecimiento', 'Actividad': 'HOSPITALES Y CLINICAS', 'DireccionUnidad': 'Negrete 1401', 'ComunaUnidad': 'San Fernando', 'RegionUnidad': 'Región del Libertador General Bernardo O´Higgins', 'Pais': 'CL', 'NombreContacto': 'Giovanni Gonzalez', 'CargoContacto': 'Administrativo Logistica', 'FonoContacto': '', 'MailContacto': ''}
+
+res_buyer = res["Listado"][0]["Comprador"]
+
+
+# {'CodigoOrganismo': '7448', 'NombreOrganismo': 'SERVICIO DE SALUD HOSPITAL DE SAN FERNANDO', 'RutUnidad': '61.602.145-1', 'CodigoUnidad': '2623', 'NombreUnidad': 'Hospital de San Fernando Abastecimiento', 'Actividad': 'HOSPITALES Y CLINICAS', 'DireccionUnidad': 'Negrete 1401', 'ComunaUnidad': 'San Fernando', 'RegionUnidad': 'Región del Libertador General Bernardo O´Higgins', 'Pais': 'CL', 'NombreContacto': 'Giovanni Gonzalez', 'CargoContacto': 'Administrativo Logistica', 'FonoContacto': '', 'MailContacto': ''}
+
+res_seller = res["Listado"][0]["Proveedor"]
+
+
+# 'Cantidad': 3, 'Listado': [{'Correlativo': 1, 'CodigoCategoria': 41116000, 'Categoria': 'Equipamiento para laboratorios / Instrumentos de medida y experimentación / Reactivos de analizadores clínicos y diagnósticos', 'CodigoProducto': 41116004, 'Producto': 'Reactivos de analizadores químicos', 'EspecificacionComprador': 'REACTIVO PARA DETERMINACIÓN DE NIVELES DE AMIKACINA (R1 2X19ML R2: 2X7 ML)\r\n', 'EspecificacionProveedor': 'REACTIVO PARA DETERMINACIÓN DE NIVELES DE AMIKACINA (R1 2X19ML R2: 2X7 ML)', 'Cantidad': 1.0, 'Unidad': None, 'Moneda': 'CLP', 'PrecioNeto': 899500.0, 'TotalCargos': 0.0, 'TotalDescuentos': 0.0, 'TotalImpuestos': 0.0, 'Total': 899500.0}, {'Correlativo': 2, 'CodigoCategoria': 41116000, 'Categoria': 'Equipamiento para laboratorios / Instrumentos de medida y experimentación / Reactivos de analizadores clínicos y diagnósticos', 'CodigoProducto': 41116004, 'Producto': 'Reactivos de analizadores químicos', 'EspecificacionComprador': 'TIRAS REACTIVAS PARA DETERMINACIÓN DE CETONEMIA (B-KETONE)(50 DETERMINACIONES C/U)', 'EspecificacionProveedor': 'TIRAS REACTIVAS PARA DETERMINACIÓN DE CETONEMIA (B-KETONE)(50 DETERMINACIONES C/U)', 'Cantidad': 4.0, 'Unidad': None, 'Moneda': 'CLP', 'PrecioNeto': 357500.0, 'TotalCargos': 0.0, 'TotalDescuentos': 0.0, 'TotalImpuestos': 0.0, 'Total': 1430000.0}, {'Correlativo': 3, 'CodigoCategoria': 41116000, 'Categoria': 'Equipamiento para laboratorios / Instrumentos de medida y experimentación / Reactivos de analizadores clínicos y diagnósticos', 'CodigoProducto': 41116004, 'Producto': 'Reactivos de analizadores químicos', 'EspecificacionComprador': 'CALIBRADOR PARA TÉCNICA AMIKACINA ( 6 NOVELES, 1X1 ML CADA UNO)', 'EspecificacionProveedor': 'CALIBRADOR PARA TÉCNICA AMIKACINA ( 6 NOVELES, 1X1 ML CADA UNO)', 'Cantidad': 1.0, 'Unidad': None, 'Moneda': 'CLP', 'PrecioNeto': 340000.0, 'TotalCargos': 0.0, 'TotalDescuentos': 0.0, 'TotalImpuestos': 0.0, 'Total': 340000.0}]}
+
+res_items = res["Listado"][0]["Items"]
+res_items_details = res["Listado"][0]["Items"]["Listado"][1]["PrecioNeto"]
+# print(d)
+
+
+
+
+
--- purchase_orders/urls.py
+++ purchase_orders/urls.py
@@ -4,8 +4,10 @@ from . import views
 # purchase_orders
 # purchase_orders/1/details
 
+app_name = 'order'
+
 urlpatterns = [
-    path("", views.index, name= "order_index"),
-    path('<int:order_id>', views.detail, name = 'order_detail')
+    path("", views.index, name= 'index'),
+    path('<int:order_id>', views.detail, name = 'detail')
     
 ]
