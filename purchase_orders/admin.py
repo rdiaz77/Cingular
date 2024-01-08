2be77f91a49b8c6c260d546a3dd774d3def5d7c2
RDM_ <rafadiazm77@gmail.com>
2024-01-07 18:00:40 -0600

    add input capabilities to admin

--- purchase_orders/admin.py
+++ purchase_orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Orders
 
 # Register your models here.
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status_code', 'name')
+
+
+
+
+admin.site.register(Orders,OrdersAdmin)
